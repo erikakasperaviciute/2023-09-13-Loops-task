e05004abed76ced9146d2d535f0547cbba27bf7b
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2023-09-13 09:40:05 +0300

    third part

--- script.js
+++ script.js
@@ -21,13 +21,43 @@
 // 5.2. Skaičiai, kurie dalinasi iš 7 ir 5 turi būti pakeisti į žodį „BuzzBiff";
 // 5.3. Skaičiai, kurie dalinasi iš 7, iš 5 ir iš 3 turi būti pakeisti į žodį „FizzBuzzBiff";
 
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+//     console.log("FizzBuzzBiff");
+//   } else if (i % 5 === 0 && i % 7 === 0) {
+//     console.log("BuzzBiff");
+//   } else if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0 && i % 7 === 0) {
+//     console.log("FizzBiff");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (i % 7 === 0) {
+//     console.log("Biff");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// 6. Skaičiai, kurie dalinasi iš 9, turi būti pakeisti į žodį „Fuzz".
+
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
-    console.log("FizzBuzzBiff");
+  if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0 && i % 9 === 0) {
+    console.log("FizzBuzzBiffFuzz");
+  } else if (i % 3 === 0 && i % 5 === 0 && i % 9 === 0) {
+    console.log("FizzBuzzFuzz");
+  } else if (i % 7 === 0 && i % 9 === 0) {
+    console.log("BiffFuzz");
+  } else if (i % 5 === 0 && i % 9 === 0) {
+    console.log("BuzzFuzz");
   } else if (i % 5 === 0 && i % 7 === 0) {
     console.log("BuzzBiff");
   } else if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
+  } else if (i % 3 === 0 && i % 9 === 0) {
+    console.log("FizzFuzz");
   } else if (i % 3 === 0 && i % 7 === 0) {
     console.log("FizzBiff");
   } else if (i % 3 === 0) {
@@ -36,6 +66,8 @@ for (let i = 1; i <= 100; i++) {
     console.log("Buzz");
   } else if (i % 7 === 0) {
     console.log("Biff");
+  } else if (i % 9 === 0) {
+    console.log("Fuzz");
   } else {
     console.log(i);
   }
